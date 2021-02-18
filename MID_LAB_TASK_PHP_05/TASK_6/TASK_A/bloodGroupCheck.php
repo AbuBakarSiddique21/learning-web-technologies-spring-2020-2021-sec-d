52e153693d191c3ba37205885c1d2e208534b674
AbuBakarSiddique21 <1993siddique3@gmail.com>
2021-02-18 21:10:53 +0600

    Task-6_Task-A_bloodGroup and bloodGroupCheck

---
+++ MID_LAB_TASK_PHP_05/TASK_6/TASK_A/bloodGroupCheck.php
@@ -0,0 +1,16 @@
+<?php
+
+	if(isset($_POST['submit']))
+	{
+		$bloodGropu = $_POST['blood'];
+		
+		if($bloodGropu == ""){
+			echo "null submission";
+		}
+		else{
+			echo "Blood group: ".$bloodGropu;
+		}
+	}else{
+		echo "Invalid request";
+	}
+?>
